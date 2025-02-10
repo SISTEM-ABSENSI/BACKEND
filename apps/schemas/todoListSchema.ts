b8b0f63d7f7ae339017f9b63ef25c3be2f5c23b0
misdarmanto <misdarmanto1922@gmail.com>
2025-02-10 21:19:25 +0700

    feat: careate todo list

---
+++ apps/schemas/todoListSchema.ts
@@ -0,0 +1,30 @@
+import Joi from 'joi'
+
+export const createTodoListSchema = Joi.object({
+  todoListName: Joi.string().max(100).required(),
+  todoListScheduleId: Joi.number().integer().positive().required(),
+  todoListStatus: Joi.boolean().optional()
+})
+
+export const updateTodoListSchema = Joi.object({
+  todoListId: Joi.number().integer().positive().required(),
+  todoListName: Joi.string().allow('').max(100).optional(),
+  todoListScheduleId: Joi.number().allow('').integer().positive().optional(),
+  todoListStatus: Joi.boolean().optional()
+})
+
+export const deleteTodoListSchema = Joi.object({
+  todoListId: Joi.number().integer().positive().required()
+})
+
+export const findOneTodoListSchema = Joi.object({
+  todoListId: Joi.number().integer().positive().required()
+})
+
+export const findAllTodoListSchema = Joi.object({
+  page: Joi.number().integer().optional(),
+  size: Joi.number().integer().optional(),
+  search: Joi.string().allow('').optional(),
+  todoListStatus: Joi.string().allow('').optional(),
+  pagination: Joi.boolean().optional()
+})
---
+++ resources/migrations/3_todoListMigration.js
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/space-before-function-paren */
+/* eslint-disable @typescript-eslint/no-var-requires */
+'use strict'
+
+const { ZygoteModel } = require('../zygote')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('todo_list', {
+      ...ZygoteModel,
+      todo_list_id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      todo_list_name: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      todo_list_schedule_id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        allowNull: false,
+        references: {
+          model: 'schedules',
+          key: 'schedule_id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      todo_list_status: {
+        type: Sequelize.BOOLEAN,
+        allowNull: true,
+        defaultValue: false
+      }
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('todo_list')
+  }
+}
